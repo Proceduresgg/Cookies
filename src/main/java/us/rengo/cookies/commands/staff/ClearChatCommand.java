51e731e6b3c763caded3950acf4317d58a9b4b20
Zaid Ali <3p6824cbrf@gmail.com>
2019-01-10 17:53:31 +0000

    use commons StringUtils#repeat

--- src/main/java/us/rengo/cookies/commands/staff/ClearChatCommand.java
+++ src/main/java/us/rengo/cookies/commands/staff/ClearChatCommand.java
@@ -3,6 +3,7 @@ package us.rengo.cookies.commands.staff;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
 import co.aikar.commands.annotation.CommandPermission;
+import org.apache.commons.lang3.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -12,12 +13,10 @@ public class ClearChatCommand extends BaseCommand {
 
     @CommandAlias("cc|clearchat")
     public void onClear(Player player) {
-        for (int x = 0; x < 101; x++) {
             Bukkit.getOnlinePlayers()
                     .stream()
                     .filter(onlinePlayer -> !onlinePlayer.hasPermission("rengo.staff"))
-                    .forEach(onlinePlayer -> onlinePlayer.sendMessage(""));
-        }
+                    .forEach(onlinePlayer -> onlinePlayer.sendMessage(StringUtils.repeat(" \n", 100)));
 
         Bukkit.broadcastMessage(ChatColor.DARK_AQUA + player.getName() + ChatColor.BLUE + " has cleared the chat.");
     }
